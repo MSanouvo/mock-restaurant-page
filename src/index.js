fed19320042d5d57c6bd8d470a370deac256bbc9
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-11-09 02:58:05 -0800

    added image folder, configured webpack
    - new folder with installed dependecies will let
    us add images to our page
    - everything relevant to this project should now
    configured properly through webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./style.css"
+import macraonIamge from "./images/macaron.jpg"
 
 console.log('Hello World')
+const image = document.createElement('img')
+image.src = macraonIamge
+document.body.appendChild(image)
