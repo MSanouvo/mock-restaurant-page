160315ddbdda2f7dbb53bff05ee3674e6aaa4515
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-11-09 21:13:12 -0800

    added content to the about page
    - starting to fill page content from the about
    section
    - page contains basic descriptor (incomplete) and
    fake contact info
    - recolored the page to not be too stand out

---
+++ src/about.js
@@ -0,0 +1,31 @@
+function displayAbout(){
+    const content = document.querySelector('#content')
+    const header = document.createElement('h1')
+    header.textContent = 'About Us'
+    const about = document.createElement('div')
+    const aboutText = document.createElement('span')
+    aboutText.textContent = 'Treating the community since XXXX. Contact us for catering or custom orders.'
+    const contact = document.createElement('div')
+    const contactNumber = document.createElement('span')
+    const contactEmail = document.createElement('a')
+    contactEmail.setAttribute('href', '')
+    const contactHeader = document.createElement('h2')
+    contactHeader.textContent = "Contact Us"
+    contactNumber.textContent = "(123) 456-7890"
+    contactEmail.textContent = "whoisthis@email.com"
+
+    about.classList.add('about')
+    contact.classList.add('contact')
+
+    content.appendChild(header)
+    about.appendChild(aboutText)
+    contact.appendChild(contactHeader)
+    contact.appendChild(contactNumber)
+    contact.appendChild(contactEmail)
+
+    content.appendChild(about)
+    content.appendChild(contact)
+
+}
+
+export {displayAbout}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import "./style.css"
 import { displayPage } from "./home"
 import { displayMenu } from "./menu"
+import { displayAbout } from "./about"
 
 displayPage()
 
+function removeChildren(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.lastChild)
+    }
+}
+
+const content = document.querySelector('#content')
 const home = document.querySelector('#home')
 const menu = document.querySelector('#menu')
 const about = document.querySelector('#about')
 
-home.addEventListener('click', displayPage)
-menu.addEventListener('click', displayMenu)
+home.addEventListener('click', () =>{
+    removeChildren(content)
+    displayPage()
+})
+menu.addEventListener('click', ()=>{
+    removeChildren(content)
+    displayMenu()
+})
+
+about.addEventListener('click', ()=>{
+    removeChildren(content)
+    displayAbout()
+})
